8c1b09fc639edb3d96a022f138978f60748f3b67
Adam Bodnar <adambodnar2@gmail.com>
2018-09-18 11:15:19 +0200

    buttons and basic layout

--- app/src/main/java/com/example/adambodnar/watchsocket/MainActivity.java
+++ app/src/main/java/com/example/adambodnar/watchsocket/MainActivity.java
@@ -2,18 +2,37 @@ package com.example.adambodnar.watchsocket;
 
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends WearableActivity {
 
-    private TextView mTextView;
+    private Button connectButton;
+    private Button sendButton;
+
+    private View.OnClickListener connectListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            System.out.println("CONNECT");
+        }
+    };
+
+    private View.OnClickListener sendListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            System.out.println("SEND");
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.text);
+        connectButton = (Button) findViewById(R.id.button_connect);
+        connectButton.setOnClickListener(connectListener);
+        sendButton = (Button) findViewById(R.id.button_send);
+        sendButton.setOnClickListener(sendListener);
 
         // Enables Always-on
         setAmbientEnabled();
